aaac4a7c951ad8cef0888e72994800dc6c693a69
Vitaly Polyakov <vit.polyakov71@gmail.com>
2025-08-30 18:08:33 +0300

    fixture

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
+import math
+
+
 def divide(a, b):
     if b > 0:
         return  a / b
     return 0
+
+
+def calculate_logarithm(x, base):
+    return math.log(x, base)
+
+
+def reverse_string(my_string):
+    return my_string[::-1]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,28 @@
-from src.main import divide
+from src.main import divide, reverse_string
+from src.calculate_logarithm import calculate_logarithm
+import pytest
 
 
 def test_divide():
     assert  divide(2, 1) == 2
 
     assert divide(2, 0) == 0
+
+
+def test_calc_log():
+    assert calculate_logarithm(8, 2) == 3.0
+    assert calculate_logarithm(8, 4) == 1.5
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(0, 2)
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(8, 0)
+
+
+def test_reverse_string_numbers(numbers):
+    assert reverse_string('123') == numbers
+
+
+def test_reverse_string_letter(letters):
+    assert reverse_string('hello') == letters
