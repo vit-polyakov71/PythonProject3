0cc7c8f3d54ab7a6bb1ae7d859158d0ba709ff78
Vitaly Polyakov <vit.polyakov71@gmail.com>
2025-08-30 17:38:19 +0300

    пример

---
+++ src/calculate_logarithm.py
@@ -0,0 +1,10 @@
+import math
+
+
+def calculate_logarithm(number):
+    if number <= 0:
+        raise ValueError("Логарифм можно вычислить только для положительных чисел")
+    return  math.log(number)
+
+# def calculate_logarithm(x, base):
+#     return math.log(x, base)
---
+++ tests/test_calc_log.py
@@ -0,0 +1,21 @@
+import pytest
+from src.calculate_logarithm import calculate_logarithm
+
+
+
+def test_calculate_logarithm_with_negative_number():
+    with pytest.raises(ValueError) as exc_info:
+        calculate_logarithm(-1)
+
+    # Проверяем, что сообщение об ошибке соответствует ожидаемому
+    assert  str(exc_info.value) == "Логарифм можно вычислить только для положительных чисел"
+
+# def test_calc_log():
+#     assert calculate_logarithm(8, 2) == 3.0
+#     assert calculate_logarithm(8, 4) == 1.5
+#
+#     with pytest.raises(ValueError):
+#         calculate_logarithm(0, 2)
+#
+#     with pytest.raises(ValueError):
+#         calculate_logarithm(8, 0)
