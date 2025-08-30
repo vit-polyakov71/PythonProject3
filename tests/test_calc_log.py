39647fd5d04eaee2baac698d74e2c5a3e1451cd9
Vitaly Polyakov <vit.polyakov71@gmail.com>
2025-08-30 17:41:05 +0300

    пример_2

--- tests/test_calc_log.py
+++ tests/test_calc_log.py
@@ -3,19 +3,19 @@ from src.calculate_logarithm import calculate_logarithm
 
 
 
-def test_calculate_logarithm_with_negative_number():
-    with pytest.raises(ValueError) as exc_info:
-        calculate_logarithm(-1)
-
-    # Проверяем, что сообщение об ошибке соответствует ожидаемому
-    assert  str(exc_info.value) == "Логарифм можно вычислить только для положительных чисел"
-
-# def test_calc_log():
-#     assert calculate_logarithm(8, 2) == 3.0
-#     assert calculate_logarithm(8, 4) == 1.5
+# def test_calculate_logarithm_with_negative_number():
+#     with pytest.raises(ValueError) as exc_info:
+#         calculate_logarithm(-1)
 #
-#     with pytest.raises(ValueError):
-#         calculate_logarithm(0, 2)
-#
-#     with pytest.raises(ValueError):
-#         calculate_logarithm(8, 0)
+#     # Проверяем, что сообщение об ошибке соответствует ожидаемому
+#     assert  str(exc_info.value) == "Логарифм можно вычислить только для положительных чисел"
+
+def test_calc_log():
+    assert calculate_logarithm(8, 2) == 3.0
+    assert calculate_logarithm(8, 4) == 1.5
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(0, 2)
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(8, 0)
